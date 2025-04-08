7d754129f840730b81a44c66c167dc85c7ee0c7c
harshi922 <harshitaay@gmail.com>
2025-04-08 10:19:39 -0400

    Added Nasa Power Upload

---
+++ Data-Upload/uploadnasapower.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Set environment variables
+set DB_USER=XXXX
+set DB_PASS=XXXX
+set DB_HOST=localhost
+set DB_PORT=5432
+set DB_NAME=covariablesv1
+
+REM Set the password for PostgreSQL using PGPASSWORD
+set PGPASSWORD=%DB_PASS%
+
+for %%f in (*.tif) do (
+    setlocal enabledelayedexpansion
+    set "basename=%%~nf"
+    set "name=np_!basename!"
+
+    REM Upload the raster file with prefixed table name
+    raster2pgsql -s 4326 -I -C -M -t 128x128 "%%f" public.!name! | psql -U !DB_USER! -d !DB_NAME! -h !DB_HOST! -p !DB_PORT!
+
+    endlocal
+)
+
+echo Done!
