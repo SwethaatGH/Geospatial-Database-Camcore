c929ed429b7af5ae45e43adc0b057d5b4c5b6def
Rcavalh <128402926+Rcavalh@users.noreply.github.com>
2025-05-07 12:10:05 -0400

    Update config.py

--- Api-v1/app/config.py
+++ Api-v1/app/config.py
@@ -1,4 +1,4 @@
 class Settings:
-    DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5433/covariablesv1"
+    DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/covariablesv1"
 
 settings = Settings()
