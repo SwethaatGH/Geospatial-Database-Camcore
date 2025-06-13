72f994c7ab876fa6aad92c6afda6c9325c291e83
mukundlogasundar <mukundlogasundar321@gmail.com>
2025-06-13 18:55:18 -0400

    Increase throughput

--- Csv-Creator/script.py
+++ Csv-Creator/script.py
@@ -194,8 +194,8 @@ async def process_csv_file(
     default_end_date=None,
     cache_file_path="climate_data_cache.json",
     selected_set=None,
-    max_concurrent=60,
-    checkpoint_size=15,      # <--- rows per output file
+    max_concurrent=100,
+    checkpoint_size=1000,      # <--- rows per output file
     checkpoint_prefix="batches/results_batch_",
     covariate_prefix="batches/covariates_batch"
 
