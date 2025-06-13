140303797ef2dc6515061238b13e5b608cd336ae
mukundlogasundar <mukundlogasundar321@gmail.com>
2025-06-13 01:00:21 -0400

    Worker increase

--- Csv-Creator/script.py
+++ Csv-Creator/script.py
@@ -179,7 +179,7 @@ async def process_csv_file(
     default_end_date=None,
     cache_file_path="climate_data_cache.json",
     selected_set=None,
-    max_concurrent=20
+    max_concurrent=50
 ):
     cache = load_cache(cache_file_path)
     df = pd.read_csv(input_csv_path)
