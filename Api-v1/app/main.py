6d3ab672ff04d6bcc203eea99014fe683abcbf10
mukundlogasundar <mukundlogasundar321@gmail.com>
2025-07-24 12:29:38 -0400

    HAND Variable Added

--- Api-v1/app/main.py
+++ Api-v1/app/main.py
@@ -75,7 +75,7 @@ AVAILABLE_VARIABLES = {
     DataSource.SPEI: ["spei"],
     DataSource.CHIRPS: ["chirps"],
     DataSource.ET: ["et"],
-    DataSource.ELEVATION: ["aspect", "elev", "flowdir", "hillshade", "roughness", "tpi", "tri", "slope"], 
+    DataSource.ELEVATION: ["aspect", "elev", "flowdir", "hillshade", "roughness", "tpi", "tri", "slope", "hand"], 
     DataSource.SOILGRIDS: ["bdod", "cec", "cfvo", "clay", "nitrogen", "ocd", "ocs", "phh2o", "sand", "silt", "soc", "wv0010", "wv0030", "wv1500"],
     DataSource.TERRACLIM: ["aet", "def", "pdsi", "pet", "ppt", "q", "soil", "srad", "tmin", "tmax", "vap", "vpd", "ws"],
     DataSource.NASAPOWER: ["airmass", "allsky_kt", "allsky_nkt", "allsky_sfc_lw_dwn", "allsky_sfc_lw_up", "allsky_sfc_par_diff", 
--- Csv-Creator/script.py
+++ Csv-Creator/script.py
@@ -73,7 +73,7 @@ DATA_SOURCES = {
     "spei": ["spei"],
     "chirps": ["chirps"],
     "et": ["et"],
-    "elev": ["aspect", "elev", "flowdir", "hillshade", "roughness", "tpi", "tri", "slope"],
+    "elev": ["aspect", "elev", "flowdir", "hillshade", "roughness", "tpi", "tri", "slope", "hand"],
     "soil": ["bdod", "cec", "cfvo", "clay", "nitrogen", "ocd", "ocs", "phh2o", "sand", "silt", "soc", "wv0010", "wv0030", "wv1500"],
     "tc": ["aet", "def", "pdsi", "pet", "ppt", "q", "soil", "srad", "tmin", "tmax", "vap", "vpd", "ws"],
     "np": ["airmass", "allsky_kt", "allsky_nkt", "allsky_sfc_lw_dwn", "allsky_sfc_lw_up", "allsky_sfc_par_diff",
