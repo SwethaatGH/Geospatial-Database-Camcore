b24d6cbe2da0caa69d473face541e35f359b01c3
mukundlogasundar <mukundlogasundar321@gmail.com>
2025-07-24 22:32:53 -0400

    Pool size change

--- Csv-Creator/script_raw.py
+++ Csv-Creator/script_raw.py
@@ -209,8 +209,8 @@ async def process_csv_file(
     default_end_date=None,
     cache_file_path="climate_data_cache.json",
     selected_set=None,
-    max_concurrent=60,
-    checkpoint_size=100,      # <--- rows per output file
+    max_concurrent=100,
+    checkpoint_size=1000,      # <--- rows per output file
     checkpoint_prefix="batches/results_batch_",
     zip_output_path = "batches/"
 ):
